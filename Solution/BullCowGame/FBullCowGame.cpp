15b127c6d7bb3668696b72611238d622a1d295fb
lesslate <39299919+lesslate@users.noreply.github.com>
2019-01-19 20:12:26 +0900

    struct

--- Solution/BullCowGame/FBullCowGame.cpp
+++ Solution/BullCowGame/FBullCowGame.cpp
@@ -28,8 +28,27 @@ void FBullCowGame::Reset()
 {
 	constexpr int32 MAX_TRIES = 8;
 	MyMaxTries = MAX_TRIES;
+
+	const FString HIDDEN_WORD = "planet";
+	MyHideenWord = HIDDEN_WORD;
+
 	MyCurrentTry = 1;
 	return;
+}
+
+BullCowCount FBullCowGame::SubmitGuess(FString)
+{
+	//incriment the turn number
+	MyCurrentTry++;
+
+	// setup a return variable
+	BullCowCount BullCowCount;
+	// loop through all letters in the guess
+		// coapre letters against the hidden word
+
+
+
+	return BullCowCount;
 }
 
 
--- Solution/BullCowGame/FBullCowGame.h
+++ Solution/BullCowGame/FBullCowGame.h
@@ -4,6 +4,13 @@
 using FString = std::string;
 using int32 = int;
 
+
+struct BullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
 class FBullCowGame {
 public:
 	FBullCowGame(); //constructor
@@ -15,8 +22,10 @@ public:
 	bool CheckGuessValidity(FString);
 	void Reset();
 
+	BullCowCount SubmitGuess(FString);
 
 private:
 	int32 MyCurrentTry;
 	int32 MyMaxTries;
+	FString MyHideenWord;
 };
